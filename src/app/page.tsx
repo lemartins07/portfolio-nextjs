8983834036645990d902dbb6be78161818f3489c
Leandro Martins <leandro.marttins.85@gmail.com>
2024-02-23 00:59:06 -0300

    Prisma configuration | Fetching data to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 import { HomeContainer, HomeContent, HomeImage } from './styles'
 import Image from 'next/image'
 import { Button } from '@/components/Button'
+import prisma from '@/lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  const user = await prisma.user.findFirst({
+    where: { name: 'Leandro Martins' },
+  })
+
   return (
-    <HomeContainer>
-      <HomeImage>
-        <Image
-          alt="Leandro Martins"
-          src="/leandro_portfolio.jpg"
-          width={700}
-          height={700}
-        />
-      </HomeImage>
-      <HomeContent>
-        <h3>hi, i am leandro martins</h3>
-        <span>front-end developer</span>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet porro
-          facere expedita ab atque a consectetur odit vel soluta itaque.
-        </p>
-        <Button href="/about" text="about me" icon="user" />
-      </HomeContent>
-    </HomeContainer>
+    user && (
+      <HomeContainer>
+        <HomeImage>
+          <Image
+            alt="Leandro Martins"
+            src="/leandro_portfolio.jpg"
+            width={700}
+            height={700}
+          />
+        </HomeImage>
+        <HomeContent>
+          <h3>hi, i am {user?.name}</h3>
+          <span>{user?.skill}</span>
+          <p>{user?.bio}</p>
+          <Button href="/about" text="about me" icon="user" />
+        </HomeContent>
+      </HomeContainer>
+    )
   )
 }
